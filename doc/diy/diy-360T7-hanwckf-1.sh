5fc424418eee4fb4b5e670d1a5e1458ae96251e4
keben12345 <79047578+keben12345@users.noreply.github.com>
2023-06-09 20:43:13 +0800

    Update diy-360T7-hanwckf-1.sh

--- doc/diy/diy-360T7-hanwckf-1.sh
+++ doc/diy/diy-360T7-hanwckf-1.sh
@@ -16,7 +16,7 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+#echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 # 添加第三方软件包
 #git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
 #git clone https://github.com/kenzok8/small-package package/small-package
